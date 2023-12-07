971f5de77addebb91c56f92a5c1c6a173fe3f2e4
nemanjaRski <nemanja.rasajski@gmail.com>
2023-12-07 18:22:46 +0100

    Added overlapping windows

--- latent_extraction/extract_beats_latents.py
+++ latent_extraction/extract_beats_latents.py
@@ -3,6 +3,7 @@ from BEATs import BEATs, BEATsConfig
 import torchaudio
 from tqdm import tqdm
 from pathlib import Path
+from utils.extract_frames import _create_windows
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -11,6 +12,7 @@ SAVE_DIRECTORY = Path(r"")
 CHECKPOINT_PATH = Path("")
 SESSIONS = ['1', '2', '3', '7']
 WINDOW = 1
+OFFSET = 1
 
 SAVE_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
@@ -28,13 +30,17 @@ def _load_model(checkpoint_path):
     return model
 
 
-def extract_latents(in_file_path, out_file_dir, model, window):
+def extract_latents(in_file_path, out_file_dir, model, window, offset):
     out_file_path = Path(out_file_dir) / in_file_path.stem
     waveform, sample_rate = torchaudio.load(str(in_file_path))
     mono_waveform = torch.mean(waveform, dim=0, keepdim=True)
-    interval_length = int(sample_rate * window)
 
-    for idx, interval_start in enumerate(tqdm(range(0, mono_waveform.shape[1], interval_length))):
+    interval_length = int(sample_rate * window)
+    interval_offset = int(sample_rate * offset)
+
+    intervals = _create_windows(mono_waveform, interval_length, interval_offset, sample_rate)
+    for idx, interval in enumerate(tqdm(intervals)):
+        interval_start = interval[0]
         with torch.no_grad():
             audio_sample = mono_waveform.squeeze()[interval_start:interval_start + interval_length].to(
                 device).unsqueeze(dim=0)
@@ -53,4 +59,4 @@ if __name__ == '__main__':
         out_subdirectory_path.mkdir(parents=True, exist_ok=True)
         print(f"Generating latents for session {in_subdirectory_name}:")
         for in_file_path in in_subdirectory_path.glob("*.wav"):
-            extract_latents(in_file_path, out_subdirectory_path, beats_model, WINDOW)
+            extract_latents(in_file_path, out_subdirectory_path, beats_model, WINDOW, OFFSET)
--- utils/extract_frames.py
+++ utils/extract_frames.py
@@ -2,44 +2,22 @@ import av
 import numpy as np
 
 
-def _sample_indices(num_total_frames: int, window: int = 1, fps: int = 30, num_to_sample: int = 16):
-    """
-    Samples gifs from video in specified time window
-    Args:
-        num_total_frames (int): total number of frames in video
-        window (int): sampling window in seconds
-        fps (int): frames per second
-        num_to_sample (int): number of frames to sample from each window
-
-    Returns:
-        result (List[List[int]]) Nested list of indices with shape [NUM_WINDOWS, num_to_sample]
-        Examples:
-            Video has 1800 frames, sampling window is 1s, fps is 30, 16 frames sampled for each window
-            result will have shape [60, 16]. NUM_WINDOWS = num_total_frames / (fps * window) = 1800 / (30 * 1) = 60
-            num_to_sample = 16
-    """
-    num_windows = round(num_total_frames / (fps * window))
-    all_indices = []
-
-    for idx in np.arange(num_windows):
-        first_frame = fps * window * idx
-        last_frame = first_frame + fps * window
-
-        if last_frame >= num_total_frames:
-            last_frame = num_total_frames - 1
-
-        indices = np.linspace(first_frame, last_frame, num=num_to_sample).astype(int)
-        all_indices.append(indices)
-
-    return all_indices
+def _create_windows(array, window=30, stride=30, sample_rate=16):
+    nrows = ((array.size - window) // stride) + 1
+    if window >= stride:
+        linspace_end = window
+    else:
+        linspace_end = stride
+    return array[stride * np.arange(nrows)[:, None] + np.linspace(0, linspace_end - 1, num=sample_rate).astype(int)]
 
 
-def sample_frames(file_path, window: int = 1, frames_to_sample: int = 16):
+def sample_frames(file_path, window: int = 1,  offset: int = 1, frames_to_sample: int = 16):
     """
     Decode the video with PyAV decoder.
     Args:
         file_path (str): path to video in string format.
         window (int): sampling window in seconds
+        offset (int): number of seconds each window should be offset by
         frames_to_sample: (int): number of frames to sample within each time window
 
     Returns:
@@ -59,6 +37,6 @@ def sample_frames(file_path, window: int = 1, frames_to_sample: int = 16):
     all_frames = np.array(list(container.decode(video=0)))
     container.close()
 
-    all_indices = _sample_indices(len(all_frames), window, fps, frames_to_sample)
+    all_indices = _create_windows(np.arange(all_frames), window * fps, offset * fps, frames_to_sample)
     all_frames = all_frames[all_indices]
     return np.stack([np.stack([x.to_ndarray(format="rgb24") for x in frames]) for frames in all_frames])
